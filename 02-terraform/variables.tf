f6a1d697f91bc139b30f4c43bc6347120ed15a63
4ubit <aubit@naver.com>
2025-08-09 02:06:26 +0900

    Create variables.tf

---
+++ 02-terraform/variables.tf
@@ -0,0 +1,36 @@
+# 인스턴스 관련
+variable "instance_name" {
+  description = "인스턴스 이름"
+  default     = "tf-instance"
+}
+
+variable "image_id" {
+  description = "이미지 UUID"
+  default     = "f033a485-cc1a-4e6d-a8c8-da6c7a059528"
+}
+
+variable "flavor_name" {
+  description = "사용할 flavor"
+  default     = "m1.medium"
+}
+
+variable "key_name" {
+  description = "SSH key pair 이름"
+  default     = "mykey"
+}
+
+variable "volume_size" {
+  description = "루트 볼륨 크기 (GiB)"
+  default     = 20
+}
+
+# 네트워크 관련
+variable "network_name" {
+  description = "내부 네트워크 이름"
+  default     = "shared"
+}
+
+variable "subnet_name" {
+  description = "내부 서브넷 이름"
+  default     = "shared-subnet"
+}
